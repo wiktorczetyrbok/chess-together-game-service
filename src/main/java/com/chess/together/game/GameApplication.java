70da31f3009bc6c1644a2175ef577d1ec3d2d700
Wiktor <wczetyrbok@griddynamics.com>
2024-01-18 13:55:34 +0100

    add initial files

---
+++ src/main/java/com/chess/together/game/GameApplication.java
@@ -0,0 +1,13 @@
+package com.chess.together.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chess/together/game/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chess.together.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
